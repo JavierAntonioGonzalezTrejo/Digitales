212301cecc88cd5b217cab3e3738824bee7104a6
ivanrm95 <ivan_martinez_lopez@hotmail.com>
2015-09-28 19:08:39 -0500

    Clase con la interface del programa
    pos eso

---
+++ src/digitales/principal/Principal.java
@@ -0,0 +1,26 @@
+
+/**
+ *
+ * @author Punto Crítiko
+ */
+public class Principal extends javax.swing.JFrame {
+
+    /**
+     * param args the command line arguments
+     */
+public Principal()
+{
+     	setResizable(false);
+        
+        this.setLocationRelativeTo(null);   
+}
+    public static void main(String[] args) {
+        // TODO code application logic here      
+                java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Interface().setVisible(true);
+            }
+        });
+    }
+
+}
